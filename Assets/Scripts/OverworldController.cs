4966f10fb82673f38420146499860b2a7b389af8
GurLad <40135047+GurLad@users.noreply.github.com>
2019-10-05 10:38:43 +0300

    OverworldController

--- Assets/Scripts/OverworldController.cs
+++ Assets/Scripts/OverworldController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class OverworldController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+    public GameObject Model;
+    public AdvancedAnimation IdleAnimation;
+    public AdvancedAnimation WalkAnimation;
+    private Rigidbody rigidbody;
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 newSpeed = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Speed;
+        if (newSpeed.magnitude >= 0.1f)
+        {
+            Model.transform.LookAt(Model.transform.position + newSpeed);
+            rigidbody.velocity = newSpeed;
+            IdleAnimation.Active = false;
+            if (!WalkAnimation.Active)
+            {
+                WalkAnimation.StartAnimations(true);
+            }
+        }
+        else
+        {
+            rigidbody.velocity = Vector3.zero;
+            WalkAnimation.Active = false;
+            if (!IdleAnimation.Active)
+            {
+                IdleAnimation.StartAnimations(true);
+            }
+        }
     }
 }
