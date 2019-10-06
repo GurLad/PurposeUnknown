8e31785fa3bc971518d281ca0dda91df611d65a5
GurLad <40135047+GurLad@users.noreply.github.com>
2019-10-06 23:11:45 +0300

    Message model and more fixes

--- Assets/ShowMessage.cs
+++ Assets/ShowMessage.cs
@@ -6,8 +6,17 @@ public class ShowMessage : MonoBehaviour
 {
     [TextArea(3,10)]
     public string Message;
+    public bool IsScanner;
+    private void Start()
+    {
+        if (IsScanner && PlayerPrefs.GetInt("GotScanner", 0) == 1)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
+        PlayerPrefs.SetInt("GotScanner", 1);
         MessageUI.Instance.Show(Message);
         Destroy(gameObject);
     }
